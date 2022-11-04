91cdee8a3da1f0d80cd53d5210eed7b8803380e4
Josh <josh@sf-iron.com>
2022-11-04 10:50:18 -0700

    Copied clean CSV files into postgres db

--- server/models/index.js
+++ server/models/index.js
@@ -24,24 +24,3 @@ module.exports = {
 
   }
 };
-
-// const { Pool } = require('pg')
-// const pool = new Pool()
-// pool.query('SELECT * FROM users WHERE id = $1', [1], (err, res) => {
-//   if (err) {
-//     throw err
-//   }
-//   console.log('user:', res.rows[0])
-// })
-
-// pool.connect((err, client, done) => {
-//   if (err) throw err
-//   client.query('SELECT * FROM users WHERE id = $1', [1], (err, res) => {
-//     done()
-//     if (err) {
-//       console.log(err.stack)
-//     } else {
-//       console.log(res.rows[0])
-//     }
-//   })
-// })
